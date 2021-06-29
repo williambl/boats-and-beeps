2735a316d3430ddcfa526cdddd4f425c45c282b3
williambl <william@williambl.com>
2021-06-29 13:43:10 +0100

    Add WEntityView widget to boat table gui

--- src/main/kotlin/com/williambl/boatsandbeeps/BoatUpgradeTableGuiDescription.kt
+++ src/main/kotlin/com/williambl/boatsandbeeps/BoatUpgradeTableGuiDescription.kt
@@ -1,49 +1,64 @@
 package com.williambl.boatsandbeeps
 
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription
+import io.github.cottonmc.cotton.gui.client.BackgroundPainter
 import io.github.cottonmc.cotton.gui.widget.WGridPanel
 import io.github.cottonmc.cotton.gui.widget.WItemSlot
 import io.github.cottonmc.cotton.gui.widget.WPlainPanel
 import io.github.cottonmc.cotton.gui.widget.WSprite
 import io.github.cottonmc.cotton.gui.widget.data.Insets
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.minecraft.entity.EntityType
+import net.minecraft.entity.passive.SheepEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.entity.vehicle.BoatEntity
 import net.minecraft.screen.ScreenHandlerContext
 import net.minecraft.util.Identifier
 
 
 class BoatUpgradeTableGuiDescription(syncId: Int, playerInventory: PlayerInventory, val context: ScreenHandlerContext) :
     SyncedGuiDescription(boatUpgradeTableScreenHandlerType, syncId, playerInventory, getBlockInventory(context, 7), getBlockPropertyDelegate(context)) {
-        init {
-            val root = WGridPanel()
-            setRootPanel(root)
-            root.setSize(140, 200)
-            root.insets = Insets.ROOT_PANEL
-            val boatSlot = WItemSlot.of(blockInventory, 0)
-            root.add(boatSlot, 1, 2)
 
-            val upgradesPanel = WPlainPanel()
-            upgradesPanel.setSize(80, 54)
+    val root: WPlainPanel = WPlainPanel()
+    val boatSlot: WItemSlot
+    val upgradesPanel: WPlainPanel
+    val upgradeSlots: Map<BoatUpgradeSlot, WItemSlot>
+    val entityView: WEntityView
 
-            val upgradeSlots = BoatUpgradeSlot.values().mapIndexed { idx, slot ->
-                slot to WItemSlot.of(blockInventory, idx+1)
-            }.toMap()
+    init {
+        setRootPanel(root)
+        root.setSize(140, 200)
+        root.insets = Insets.ROOT_PANEL
+        boatSlot = WItemSlot.of(blockInventory, 0)
+        root.add(boatSlot, 18, 36)
 
-            upgradesPanel.add(WSprite(boatSprite), 0, 2, 80, 50)
+        upgradesPanel = WPlainPanel()
+        upgradesPanel.setSize(80, 54)
 
-            upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.AFT], 0, 18)
-            upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.BACK], 18, 18)
-            upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.PORT], 29, 36)
-            upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.STARBOARD], 29, 0)
-            upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.FRONT], 41, 18)
-            upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.BOW], 59, 18)
+        upgradeSlots = BoatUpgradeSlot.values().mapIndexed { idx, slot ->
+            slot to WItemSlot.of(blockInventory, idx+1)
+        }.toMap()
 
-            root.add(upgradesPanel, 4, 1)
+        upgradesPanel.add(WSprite(boatSprite), 0, 2, 80, 50)
 
-            root.add(this.createPlayerInventoryPanel(), 0, 6)
+        upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.AFT], 0, 18)
+        upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.BACK], 18, 18)
+        upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.PORT], 29, 36)
+        upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.STARBOARD], 29, 0)
+        upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.FRONT], 41, 18)
+        upgradesPanel.add(upgradeSlots[BoatUpgradeSlot.BOW], 59, 18)
 
-            root.validate(this)
-        }
+        root.add(upgradesPanel, 77, 18)
+
+        entityView = WEntityView(BoatEntity(EntityType.BOAT, world))
+        root.add(entityView, 72, 72, 90, 36)
+
+        root.add(this.createPlayerInventoryPanel(), 0, 108)
+
+        root.validate(this)
+    }
 
     override fun close(playerEntity: PlayerEntity?) {
         super.close(playerEntity)
