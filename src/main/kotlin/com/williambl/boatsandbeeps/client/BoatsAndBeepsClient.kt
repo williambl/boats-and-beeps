fae20434c7ca3b21712d05ed2ec5e410983b0b41
williambl <william@williambl.com>
2021-07-04 23:51:51 +0100

    Add """texture""" for upgrade table

--- src/main/kotlin/com/williambl/boatsandbeeps/client/BoatsAndBeepsClient.kt
+++ src/main/kotlin/com/williambl/boatsandbeeps/client/BoatsAndBeepsClient.kt
@@ -1,13 +1,17 @@
 package com.williambl.boatsandbeeps.client
 
+import com.williambl.boatsandbeeps.boatUpgradeTable
 import com.williambl.boatsandbeeps.boatUpgradeTableScreenHandlerType
 import com.williambl.boatsandbeeps.table.BoatUpgradeTableGuiDescription
 import com.williambl.boatsandbeeps.upgradedBoatEntityType
 import com.williambl.boatsandbeeps.upgradedBoatItems
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry
+import net.minecraft.block.BlockRenderType
+import net.minecraft.client.render.RenderLayer
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
@@ -15,6 +19,7 @@ import org.apache.http.impl.io.IdentityInputStream
 
 
 fun init() {
+    BlockRenderLayerMap.INSTANCE.putBlock(boatUpgradeTable, RenderLayer.getCutoutMipped())
     EntityRendererRegistry.INSTANCE.register(upgradedBoatEntityType) { context -> UpgradedBoatRenderer(context) }
     ScreenRegistry.register(boatUpgradeTableScreenHandlerType) { gui: BoatUpgradeTableGuiDescription, inventory: PlayerInventory, title: Text ->
         BoatUpgradeTableScreen(
