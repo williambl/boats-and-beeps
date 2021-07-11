92e9f1ca19d400b4ddfaeee480e1c35a04a88fae
williambl <william@williambl.com>
2021-07-12 00:52:48 +0100

    Fix mixins in prod

--- src/main/java/com/williambl/boatsandbeeps/mixin/BoatEntityMixin.java
+++ src/main/java/com/williambl/boatsandbeeps/mixin/BoatEntityMixin.java
@@ -4,20 +4,25 @@ import com.williambl.boatsandbeeps.boat.UpgradedBoatEntity;
 import net.minecraft.entity.vehicle.BoatEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BoatEntity.class)
 public class BoatEntityMixin {
     @Shadow private float velocityDecay;
 
+    @Group(name = "boatsAndBeeps$dontPushAwayMultipartBoats")
     @Redirect(method = "pushAwayFrom", at=@At(value = "CONSTANT", args = "classValue=net/minecraft/entity/vehicle/BoatEntity"))
-    boolean boatsAndBeeps$dontPushAwayMultipartBoats(Object obj, Class<?> clazz) {
+    boolean boatsAndBeeps$dontPushAwayMultipartBoats$dev(Object obj, Class<?> clazz) {
         return obj instanceof BoatEntity && !(obj instanceof UpgradedBoatEntity);
     }
+
+    @Group(name = "boatsAndBeeps$dontPushAwayMultipartBoats")
+    @Redirect(method = "pushAwayFrom", at=@At(value = "CONSTANT", args = "classValue=net/minecraft/class_1690"))
+    boolean boatsAndBeeps$dontPushAwayMultipartBoats$prod(Object obj, Class<?> clazz) {
+        return obj instanceof BoatEntity && !(obj instanceof UpgradedBoatEntity);
+    }
+
 
     @Inject(method = "updateVelocity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;getVelocity()Lnet/minecraft/util/math/Vec3d;", ordinal = 1))
     void changeVelocityDecay(CallbackInfo ci) {
